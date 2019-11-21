33dd70bea98b74b1b7ffb27b2940705f46f886b1
zema <zlobzn@gmail.com>
2019-11-21 20:36:58 +0200

    scaling example

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const containerId = process.env.HOSTNAME;
+
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send(`Hello World from <code>${containerId}</code>!`);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Example app ${containerId} listening on port ${port}!`);
+});
